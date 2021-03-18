187b0f8edbcfe8f62e7b3d403969c7ab975e22b5
AresTran <thanhan311@gmail.com>
2021-03-18 20:10:25 +0700

    antt_update_20210318_2010

--- BookManagement/src/controller/DataController.java
+++ BookManagement/src/controller/DataController.java
@@ -8,6 +8,8 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DataController {
@@ -22,6 +24,22 @@ public class DataController {
             bufferedWriter = new BufferedWriter(fileWriter);
             printWriter = new PrintWriter(bufferedWriter);
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void openFileToRead(String fileName) {
+        try {
+            scanner = new Scanner(Paths.get(fileName), "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeFileAfterRead(String fileName) {
+        try {
+            scanner.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -44,6 +62,77 @@ public class DataController {
         openFileToWrite(fileName);
         printWriter.println(brm.getReader().getReaderID() + "|" + brm.getBook().getBookID() + "|"
                 + brm.getNumOfBorrow() + "|" + brm.getState() + "|" + brm.getTotalBorrowed());
+    }
+
+    public ArrayList<Reader> readReadersFromFile(String fileName) {
+        openFileToRead(fileName);
+        ArrayList<Reader> readers = new ArrayList<>();
+        while (scanner.hasNext()) {
+            String data = scanner.nextLine();
+            Reader reader = createReaderFromData(data);
+            readers.add(reader);
+        }
+
+        closeFileAfterRead(fileName);
+        return readers;
+    }
+
+    public Reader createReaderFromData(String data) {
+        String[] datas = data.split("\\|");
+        //printWriter.println(reader.getReaderID() + "|" + reader.getFullName() + "|" + reader.getAddress() + "|"
+        //                + reader.getPhoneNumber());
+        //Reader(int readerID, String fullName, String address, String phoneNumber)
+
+        Reader reader = new Reader(Integer.parseInt(datas[0]), datas[1], datas[2], datas[3]);
+        return reader;
+    }
+
+    public ArrayList<Book> readBooksFromFile(String fileName) {
+        openFileToRead(fileName);
+        ArrayList<Book> books = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String data = scanner.nextLine();
+            Book book = createBookFromData(data);
+            books.add(book);
+        }
+        closeFileAfterRead(fileName);
+        return books;
+    }
+
+    private Book createBookFromData(String data) {
+        String[] datas = data.split("\\|");
+        //printWriter.println(book.getBookID() + "|" + book.getBookName() + "|" + book.getAuthor() + "|"
+        //                + book.getSpecialization() + "|" + book.getPublicYear() + "|" + book.getQuantity());
+
+        //Book(int bookID, String bookName, String author, String specialization, int publicYear, int quantity)
+        Book book = new Book(Integer.parseInt(datas[0]), datas[1], datas[2], datas[3],
+                Integer.parseInt(datas[4]), Integer.parseInt(datas[5]));
+        return book;
+    }
+
+    public ArrayList<BookReaderManagement> readBRMsFromFile(String fileName) {
+        openFileToRead(fileName);
+        ArrayList<BookReaderManagement> brms = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String data = scanner.nextLine();
+            BookReaderManagement reader = createBRMFromData(data);
+            brms.add(reader);
+        }
+        closeFileAfterRead(fileName);
+        return brms;
+    }
+
+    public BookReaderManagement createBRMFromData(String data) {
+        String[] datas = data.split("\\|");
+        //  printWriter.println(brm.getReader().getReaderID() + "|" + brm.getBook().getBookID() + "|"
+        //                + brm.getNumOfBorrow() + "|" + brm.getState() + "|" + brm.getTotalBorrowed());
+
+        //BookReaderManagement(Book book, Reader reader, int numOfBorrow, String state, int totalBorrowed)
+        BookReaderManagement brm = new BookReaderManagement(new Book(Integer.parseInt(datas[1])),
+                new Reader(Integer.parseInt(datas[1])), Integer.parseInt(datas[2]), datas[3], 0);
+        // xem tiếp P4: Thêm mới và hiển thị thông tin Books
+
+        return brm;
     }
 
     void closeFileAfterWrite(String fileName) {
